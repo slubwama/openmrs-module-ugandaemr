d07588e9ec18b50b464db6f4145be2e4ac9f699c
Lubwama Samuel <lubwamasamuel3@gmail.com>
2026-07-10 21:52:17 +0300

    Resolve code standard violations in PR (#899)

--- api/src/main/java/org/openmrs/module/ugandaemr/api/db/hibernate/HibernateUgandaEMRDAO.java
+++ api/src/main/java/org/openmrs/module/ugandaemr/api/db/hibernate/HibernateUgandaEMRDAO.java
@@ -13,7 +13,11 @@
  */
 package org.openmrs.module.ugandaemr.api.db.hibernate;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
